8f2bbc69e19543ab995c0a64bf279e27c67b0594
David Chung <davidc4747@yahoo.com>
2018-03-06 15:52:35 -0800

    Display main view

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const { app, BrowserWindow } = require('electron');
+
+let win;
+app.on('ready', function () {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+
+
+    win.loadURL(`file://${__dirname}/index.html`);
+});
